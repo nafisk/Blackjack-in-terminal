77b64485d68b81d7eac3a2e1a89a33cc784a7cf4
Nafis Khan <nafisrizwank@gmail.com>
2023-02-05 18:03:47 -0500

    implemended player class

--- Game.py
+++ Game.py
@@ -1,3 +1,8 @@
+import os
+from time import sleep
+from subprocess import call
+
+
 class Game:
 
     MAX_HIT_SCORE = 21
@@ -13,4 +18,23 @@ class Game:
         pass
 
     def clear_screen(self):
-        pass
+        # clear screen for specific operating system
+        _ = call('clear' if os.name == 'posix' else 'clear')
+
+    def print_welcome(self):
+        print('Welcome to Blackjack!')
+        sleep(1)
+        print('Dealing cards...')
+        sleep(1)
+
+    def get_player_name(self):
+        return input('What is your name? ')
+
+    def print_player_hand(self, player):
+        print(f'{player.name}\'s hand: {player.hand} ({player.get_score()})')
+
+
+game = Game()
+print('hello geeks\n'*10)
+
+game.clear_screen()
