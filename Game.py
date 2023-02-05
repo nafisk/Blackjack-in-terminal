dab5c611a8c40c0e990ecfa44be18adc823f4683
Nafis Khan <nafisrizwank@gmail.com>
2023-02-05 15:20:47 -0500

    implemended player class

--- Game.py
+++ Game.py
@@ -1,19 +1,16 @@
 class Game:
-    
-    GAME_BUST_SCORE = 21
+
+    MAX_HIT_SCORE = 21
     DEALER_STAND_SCORE = 17
-    
+
     def init(self):
         pass
-            
+
     def play_blackjack(self):
         pass
-    
+
     def play_again(self):
         pass
-    
+
     def clear_screen(self):
         pass
-    
-    
-        
---
+++ Player.py
@@ -0,0 +1,64 @@
+import Cards
+import itertools
+
+
+class Player:
+
+    # setup member variables
+    def __init__(self, name, max_score):
+        self.name = name
+        self.hand = []
+        self.MAX_SCORE = max_score
+        self.card_suite = Cards.Cards()
+
+    # add card to player's hand
+    def add_card(self, card):
+        self.hand.append(card)
+
+    # get player's hand
+    def get_hand(self):
+        return self.hand
+
+    # get player's name
+    def get_name(self):
+        return self.name
+
+    # get player's total score
+    def get_score(self):
+        score = 0
+        number_of_aces = 0
+
+        # get score of hand
+        for card in self.hand:
+            if card == 'A':
+                number_of_aces += 1
+            else:
+                score += self.card_suite.get_card_value(card)
+
+        # handle multiple aces in hand
+        if number_of_aces > 0:
+            score = self.handle_aces(score, number_of_aces)
+
+        return score
+
+    # handle multiple aces in hand with given score
+    def handle_aces(self, score, number_of_aces):
+
+        # score + optimal ace value
+        optimal_score = score
+
+        # get all possible ace values
+        ace_points = self.card_suite.get_card_values('A').split('|')
+
+        # combination of ace values as a list // [[1, 11], [1, 11], ...]
+        ace_list = number_of_aces * [list(map(int, ace_points))]
+
+        # get all possible combinations of ace values and set hightest score <= 21
+        for combination in itertools.product(*ace_list):
+            if sum(combination) + score <= self.MAX_SCORE:
+                optimal_score = sum(combination) + score
+
+        return optimal_score
+
+
+player = Player('Nafis', 21)
